9dc7b0d4751220ad323dc230d4141532b665311a
emrhnozk <emirozkan61@hotmail.com>
2022-10-01 15:46:01 +0300

    wip-3 //added underlining

--- src/components/TextPart.js
+++ src/components/TextPart.js
@@ -10,12 +10,12 @@ export default function TextPart() {
     margin: "0px",
     userSelect: "none",
     webkitUserSelect: "none",
-    opacity: "40%",
     fontFamily: "Source Code Pro",
     fontStyle: "normal",
     fontWeight: "500",
     fontSize: "24px",
-    color: "rgba(255, 255, 255, 0.65)",
+    color: "#FFFFFFA5",
+    color: "#6e7779",
   };
 
   const blink = keyframes`
@@ -42,8 +42,15 @@ export default function TextPart() {
     letters.push(currentLetters);
   });
 
+  //Shuffle array
+  letters = letters.sort((a, b) => 0.5 - Math.random());
+
   letters.forEach((element) => {
-    var reMappedLetters = element.map((letter) => <letter style={{ ...fontStyle }}>{letter}</letter>);
+    var reMappedLetters = element.map((letter) => (
+      <letter data-istrue="false" style={{ ...fontStyle }}>
+        {letter.toLowerCase()}
+      </letter>
+    ));
     LettersToRender.push(
       <Box item id={`${letters.indexOf(element)}word`} sx={{ margin: "0px", height: "30px" }}>
         {reMappedLetters}
@@ -62,9 +69,32 @@ export default function TextPart() {
   const linesPassed = React.useRef(1);
 
   var currentWord = LettersToRender[0];
-  var currentLetter = currentWord.props.children[indexOfIndex.current].props.children;
+  var currentLetter = currentWord.props.children[indexOfIndex.current];
+
+  var lastInputForAndroid = "";
+
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+    }
+
+    return "unknown";
+  }
 
   React.useEffect(() => {
+    nextRow.current = calculateWordsInARow();
     document.getElementById(`${indexOfWord.current}word`).prepend(refIndex.current);
   }, []);
 
@@ -72,6 +102,7 @@ export default function TextPart() {
     document.getElementById(`${indexOfWord.current}word`).prepend(refIndex.current);
     indexOfIndex.current = 0;
     refIndex.current.style.marginLeft = `${indexOfIndex.current * 14.41 + "px"}`;
+    currentLetter = currentWord.props.children[indexOfIndex.current];
   }
 
   function scrollText() {
@@ -81,8 +112,7 @@ export default function TextPart() {
   function moveIndex() {
     indexOfIndex.current = indexOfIndex.current + 1;
     refIndex.current.style.marginLeft = `${indexOfIndex.current * 14.41 + "px"}`;
-    currentLetter = currentWord.props.children[indexOfIndex.current].props.children;
-    console.log(currentLetter);
+    currentLetter = currentWord.props.children[indexOfIndex.current];
   }
 
   const rowCalculation = React.useRef(0);
@@ -104,12 +134,17 @@ export default function TextPart() {
   }
 
   function handleAnswerChange(event) {
-    if (event.key === " ") {
+    var letterHTML = document.getElementById(`${indexOfWord.current}word`).children[indexOfIndex.current + 1];
+
+    if (event.code == "Space") {
       //If not any letter is written, don't accept the space input.
       if (refInput.current.value === "" || refInput.current.value === " ") {
         refInput.current.value = "";
         return;
       }
+
+      //Calculate next row.
+      nextRow.current = calculateWordsInARow();
 
       if (indexOfWord.current == nextRow.current - 1 + rowCalculation.current) {
         console.log("next row!");
@@ -123,6 +158,26 @@ export default function TextPart() {
         }
       }
 
+      //If there's error in a word underline letters.
+      var parentElementHTML = letterHTML.parentElement;
+      //// letterCoundOnAWord has a index element so there is one more element.
+      var letterCountOnAWord = parentElementHTML.childElementCount;
+      var tempTrues = 0;
+      for (let i = 1; i < letterCountOnAWord; i++) {
+        if (parentElementHTML.children[i].getAttribute("data-istrue") == "true") {
+          tempTrues++;
+        }
+      }
+
+      if (tempTrues == letterCountOnAWord - 1) {
+        console.log("word true");
+      } else {
+        for (let i = 1; i < letterCountOnAWord; i++) {
+          parentElementHTML.children[i].style.textDecoration = "underline";
+          parentElementHTML.children[i].style.textDecorationColor = "#d6646f";
+        }
+      }
+
       //Increment index and go to the next word.
       indexOfWord.current++;
       currentWord = LettersToRender[indexOfWord.current];
@@ -131,15 +186,21 @@ export default function TextPart() {
       return;
     }
 
+    //Handle wrong and true key hit.
+    if (event.key === currentLetter.props.children.toLowerCase()) {
+      //Get the current word
+      letterHTML.style.color = "#FFFFFFA5";
+      letterHTML.setAttribute("data-istrue", "true");
+    } else {
+      letterHTML.style.color = "#d6646f";
+    }
+
     //If word ended handle it here.
     if (currentWord.props.children.length - 1 == indexOfIndex.current) {
       console.log("word ended");
       refIndex.current.style.marginLeft = `${(indexOfIndex.current + 1) * 14.41 + "px"}`;
       return;
     }
-
-    nextRow.current = calculateWordsInARow();
-    console.log(nextRow.current, rowCalculation.current);
     //Move index 1 increment.
     moveIndex();
   }
@@ -153,7 +214,7 @@ export default function TextPart() {
         container
         className="text-container"
       >
-        <div ref={refIndex} style={{ margin: "0px", width: "2px", height: "30px", position: "absolute", backgroundColor: "#B0C4B1" }} />
+        <div ref={refIndex} className="indexer" style={{ margin: "0px", width: "2px", height: "30px", position: "absolute", backgroundColor: "#B0C4B1" }} />
         {LettersToRender}
       </Grid>
     </div>
